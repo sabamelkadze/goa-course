d5d51582cdb6f302240c8ae552b29d97a08a5f39
sabamelkadze <sabamelkadze007@gmail.com>
2024-02-03 19:25:16 +0400

    second commit

---
+++ day 1/homework/lesson1.py
@@ -0,0 +1,7 @@
+name = "saba"
+surname = "melkadze"
+age = "14"
+height = "178"
+story = "saba likes playing basketball with his friends"
+print(name + " " + str(surname) + " " + str(age) + " " + str(height) + " " + str(story))
+
